fc8dc267a8cbf499645c9364cb8dc2fa5adc8286
jalopez19 <arturo.valleycharter@gmail.com>
2022-09-09 16:03:40 -0600

    Subir website
    He subido todos mis archivos

---
+++ js/questions.js
@@ -0,0 +1,21 @@
+(function(){
+    const tittleQuestions = [...document.querySelectorAll('.questions__tittle')];
+    console.log(tittleQuestions)
+    
+    tittleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement; 
+
+            addPadding.classList.toggle('questions__padding--add');
+            question.children[0].classList.toggle('questions__arrow--rotate');
+        
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height =`${height}px`;
+        });
+    });
+})();
